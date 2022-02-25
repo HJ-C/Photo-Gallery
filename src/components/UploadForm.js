26e409af09a6fe7f899193c2f9e68b2bac5c13a8
HJ-C <swch56@naver.com>
2022-02-25 23:50:26 +0900

    error code

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
 
+    const [file, setFile] = useState(null)
+    const [error, setError] = useState(null)
+    
+    const types = ['image/png', 'image/jpg']
+
+    const changeHandler = e =>{
+        let selected = e.target.files[0]
+
+        if(selected && types.includes(selected.type)) {
+            setFile(selected)
+        }else{
+            setFile(null)
+            setError('png 또는 jpg로 선택해주세요.')
+        }
+    }
 
     return (
         <form>
-            <input type ="file"></input>
+            <input type ="file" onChange={changeHandler}></input>
+            <div className="output">
+                {error && <div className="error">{error}</div> }
+                {file && <div>{file.name}</div>}
+            </div>
         </form>
     )
 }
