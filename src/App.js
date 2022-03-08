faadee12e79310bfd03332869ddcec0371279a95
HJ-c <swch56@naver.com>
2022-03-08 23:20:08 +0900

    #6

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Title from './components/title';
 import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import useFirestore from '../hooks/useFirestore';
-import { motion } from 'framer-motion';
 
-const ImageGrid = ({ setSelectedImg }) => {
-  const { docs } = useFirestore('images');
+const Modal = ({ setSelectedImg, selectedImg }) => {
+
+  const handleClick = (e) => {
+    if (e.target.classList.contains('backdrop')) {
+      setSelectedImg(null);
+    }
+  }
 
   return (
-    <div className="img-grid">
-      {docs && docs.map(doc => (
-        <motion.div className="img-wrap" key={doc.id} 
-          layout
-          whileHover={{ opacity: 1 }}s
-          onClick={() => setSelectedImg(doc.url)}
-        >
-          <motion.img src={doc.url} alt="uploaded pic"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1 }}
-          />
-        </motion.div>
-      ))}
+    <div className='backdrop' onClick={handleClick}>
+      <img src={selectedImg}></img>  
     </div>
   )
 }
 
-export default ImageGrid;
+export default Modal;
